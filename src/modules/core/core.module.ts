8488073dc870d2b4f4415d0f176bde0043144306
Sam <sammobrowne@gmail.com>
2024-08-17 22:39:44 +0800

    wip

--- src/modules/core/core.module.ts
+++ src/modules/core/core.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: '**', redirectTo: '/home' }, // Wildcard route for a 404 page
+];
 
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HomeComponent],
+  imports: [RouterModule.forRoot(routes), CommonModule],
 })
-export class CoreModule { }
+export class CoreModule {}
