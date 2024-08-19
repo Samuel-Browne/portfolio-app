69d43514ed1a1969b17107e561f54d5d766b42bc
Sam <sammobrowne@gmail.com>
2024-08-19 19:54:32 +0800

    nintendo DS bios font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        nintendo: ["Nintendo DS", "sans-serif"],
+      },
+    },
   },
+
   plugins: [],
 };
